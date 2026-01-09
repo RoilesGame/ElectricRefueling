6d8dd93917e8c2fd24cf2db3e8a304a6dda539d8
RoilesGame <118464417+RoilesGame@users.noreply.github.com>
2026-01-09 22:54:26 +0300

    Needed parameters for the electric car

--- ElectricRefueling/ElectricCar.cs
+++ ElectricRefueling/ElectricCar.cs
@@ -1,12 +1,18 @@
 namespace ElectricRefueling;
 
+[Serializable]
 public class ElectricCar
 {
-    public Car()
+    public ElectricCar(string model, int currentEnergyLevel, int batteryCapacity, int energyConsumption)
     {
-        
+        Model = model;
+        CurrentEnergyLevel = currentEnergyLevel;
+        BatteryCapacity = batteryCapacity;
+        EnergyConsumption = energyConsumption;
     }
-    
+
     public string Model { get; set; }
-    public string Connector { get; set; }
+    public int CurrentEnergyLevel { get; set; }
+    public int BatteryCapacity  { get; set; }
+    public int EnergyConsumption { get; set; }
 }
