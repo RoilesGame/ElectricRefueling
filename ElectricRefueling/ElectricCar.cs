f902b33bb93130da5e36bb018337545d3b831233
RoilesGame <118464417+RoilesGame@users.noreply.github.com>
2026-01-09 22:52:14 +0300

    Initial commit

---
+++ ElectricRefueling/ElectricCar.cs
@@ -0,0 +1,12 @@
+namespace ElectricRefueling;
+
+public class ElectricCar
+{
+    public Car()
+    {
+        
+    }
+    
+    public string Model { get; set; }
+    public string Connector { get; set; }
+}
