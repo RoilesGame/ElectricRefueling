b87eeda4d8c8848b852655570efaa89b04a9cf9e
RoilesGame <118464417+RoilesGame@users.noreply.github.com>
2026-01-12 01:22:08 +0300

    Universal dataset parser with data containers for the parsed datasets. Data containers will be updated every 60 minutes. Also added a summary for the documentation.

---
+++ ElectricRefueling/RoadWorkData.cs
@@ -0,0 +1,18 @@
+namespace ElectricRefueling;
+
+public class RoadWorkData
+{
+    // Добавьте здесь свойства с именами, соответствующими полям из JSON
+    // Например, после просмотра структуры вы увидите поля типа:
+    // public int Number { get; set; }
+    // public string Name { get; set; } = string.Empty;
+    // public string Address { get; set; } = string.Empty;
+    // и т.д.
+    
+    // Временная заглушка для компиляции
+    public override string ToString()
+    {
+        return "RoadWorkData - заполните класс после просмотра структуры JSON";
+    }
+}
+
